795d55dd473c3a45c3fc09b8542318426e73db89
Vladyslav Kalischuk <elektronikerv@gmail.com>
2017-08-15 13:30:34 +0300

    Update UploadImage.java

--- src/UploadImage.java
+++ src/UploadImage.java
@@ -15,7 +15,7 @@ import java.util.List;
 @WebServlet(name = "UploadImage", urlPatterns = "/uploadImage")
 public class UploadImage extends HttpServlet {
 
-    private static final String IMAGE_DIRECTORY = "/home/elektroniker/JavaPractice/Web/InstClone/web/images/";
+    private static final String IMAGE_DIRECTORY =""; //full path to the image directory
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
